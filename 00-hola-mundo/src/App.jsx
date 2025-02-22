4f3de316a5f177c5f1d88548324f776effe4f643
Kevin-Mendoza07 <kevingfx71@gmail.com>
2025-02-21 20:18:05 -0600

    Add first react project

---
+++ 00-hola-mundo/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+export function App(){
+    return(
+       <article>
+        <header>
+            <img src="https://unavatar.io/midudev" alt="El avatar de midudev"/>
+            <div>
+                <strong>Miguel Ángel Duran</strong>
+                <span>@midudev</span>
+            </div>
+        </header>
+        <aside>
+            <button>Seguir</button>
+        </aside>
+       </article>
+    )
+}
